ddd2f391db969c5610dbf171ef7ff436bbd08f4d
Alexandra Holstensson <ro24nz8c@students.codeinstitute.net>
2024-12-09 14:21:59 +0000

    add some jquery effects

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+    $("#stream1_btn").on(function() {
+        // show effect. can use hide the same way
+        /**$(".stream1").show();
+        $(".stream1").show('slow');
+        $(".stream1").show('medium');
+        $(".stream1").show('fast');
+        $(".stream1").show(1000);**/
+        
+        // toggle
+        /*$(".stream1").toggle();
+        $(".stream1").toggle(1000);*/
+        
+        // slideDown, can use slideUp and slideToggle, fadeIn, fadeOut, fadeToggle the same way 
+        /*$(".stream1").slideDown();
+        $(".stream1").slideDown(1000);*/
+        
+        // fadeTo
+        /*$(".stream1").fadeTo(1000, 0.5); //first parameter time in milliseconds, second parameter opacity */
+        
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
