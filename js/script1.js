2573e94d5f27fa7b5dc591242dad0bf55cfbe349
Alexandra Holstensson <ro24nz8c@students.codeinstitute.net>
2024-12-10 10:54:55 +0000

    Add some scripts

---
+++ js/script1.js
@@ -0,0 +1,10 @@
+/* Script 1 - index2.html */
+/* Script 1.1 - slideToggle */
+/*$("p").click(function() {
+    $("p").slideToggle("slow"); /* makes both paragraphs disappear */
+/*});*/
+
+/* Script 1.2 - slideToggle*/
+/*$("p").click(function() {
+    $(this).slideToggle("slow"); /* makes the paragraph that is being clicked on disappear */
+/*});*/
---
+++ js/script2.js
@@ -0,0 +1,8 @@
+/* Script 2 - index3.html, style2.css */
+/*$("button").mouseenter(function() {
+    $(this).removeClass("makeRed").addClass("makeBlue"); 
+});
+
+$("button").mouseleave(function() {
+    $(this).removeClass("makeBlue").addClass("makeRed"); 
+});*/
