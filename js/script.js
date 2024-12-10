ba3cf2acfe8cef90ecf5adbbbe7171fcb6f3be3b
Alexandra Holstensson <ro24nz8c@students.codeinstitute.net>
2024-12-10 08:24:55 +0000

    Add some files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$("p").click(function() {
+    //$("p").slideToggle("slow"); /* makes both paragraphs disappear */
+    $(this).slideToggle("slow"); /* makes the paragraph that is being clicked on disappear */
+});
+
